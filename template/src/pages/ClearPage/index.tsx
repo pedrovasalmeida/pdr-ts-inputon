9a2d765a7df183014ec4ccf228a415bbe319716c
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-04 00:47:34 -0300

    adding new clean page

---
+++ template/src/pages/ClearPage/index.tsx
@@ -0,0 +1,15 @@
+/** Packages */
+import React from 'react';
+
+/** My Components */
+import { Container } from './styles';
+
+const ClearPage: React.FC = () => {
+  return (
+    <Container>
+      <span>ClearPage</span>
+    </Container>
+  );
+};
+
+export default ClearPage;
